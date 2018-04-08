718afb74f735d5b1cf726da4d35bc38b2eedd0b1
xishuang <cairuchao@linghit.com>
2018-04-08 19:04:31 +0800

    2018-04-08 修改了下注释

--- app/src/main/java/com/xishuang/imageviewprocess/algorithm/SpecialColorMatrix.java
+++ app/src/main/java/com/xishuang/imageviewprocess/algorithm/SpecialColorMatrix.java
@@ -4,10 +4,6 @@ package com.xishuang.imageviewprocess.algorithm;
  * Author:xishuang
  * Date:2018.04.05
  * Des:通过特定的过滤算法实现的ColorMatrix
- * [  a(0), b(1), c(2), d(3), e(4),
- * f(5) ,g(6), h(7), i(8), j(9),
- * k(10),l(11),m(12),n(13),o(14),
- * p(15),q(16),r(17),s(18),t(19)]
  */
 public class SpecialColorMatrix {
 
@@ -38,7 +34,7 @@ public class SpecialColorMatrix {
     };
 
     /**
-     * 底片怀旧效果
+     * 底片反转效果
      * <p>
      * R = 255 - 1*R;
      * G = 255 - 1*G;
